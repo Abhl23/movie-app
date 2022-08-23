70ac33d2846c3b6e57f759f3b625f9bbf191b47f
Abhi <bhatt23abhishek@gmail.com>
2022-08-23 20:57:09 +0530

    dispatch action

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import { createStore } from 'redux';
-import movies from './reducers';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
 
-const store=createStore(movies);
-console.log('store', store);
-console.log('state', store.getState());
+import "./index.css";
+import App from "./components/App";
+import movies from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(movies);
+console.log("store", store);
+console.log("before state", store.getState());
+
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "Dark Knight" }],
+});
+
+console.log("after state", store.getState());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
